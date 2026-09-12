711d66eb03a6062a929ea9ef53812a64605b9dbb
Matt <50807277+ImMattic@users.noreply.github.com>
2026-09-12 00:32:55 -0600

    testing github actions

--- frontend/components/ui/NavBar.tsx
+++ frontend/components/ui/NavBar.tsx
@@ -11,7 +11,7 @@ import ThemeMenu from "./ThemeMenu";
 const NAV_LINKS = [
   { href: "/", label: "Live Map" },
   { href: "/dashboard", label: "Dashboard" },
-  { href: "/trips", label: "Trips" },
+  { href: "/trips", label: "Tripss" },
 ];
 
 export default function NavBar() {
